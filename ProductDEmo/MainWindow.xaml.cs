53bdd6fb550b65ece983d80e4fe47cbf388c0099
Your Name <vbnf000@Mail.ru>
2023-04-29 11:33:36 +0300

    First

---
+++ ProductDEmo/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProductDEmo
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static DataBaseEntities db = new DataBaseEntities();
+        public static MainWindow mainWindow;
+        public static ProductWindow productWindow = new ProductWindow();
+        public static CaptchaWindow captchaWindow = new CaptchaWindow();
+        public static int NumbersOfIncorrectAuthorizations = 0;
+        public static int CaptchaNumbers = 00000;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainWindow = this;
+            PathInitializa();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void GenerateCaptcha()
+        {
+            Random rnd = new Random();
+            CaptchaNumbers = rnd.Next(10000, 99999);
+        }
+        public void PathInitializa()
+        {
+            string vivod = "";
+            foreach(var Product in db.Product)
+            {
+                if (Product.ProductPhoto != null)
+                {
+                    Product.FullPath = "Resources/" + Product.ProductPhoto;
+                }
+
+                vivod += Product.FullPath + "\n";
+            }
+          //  MessageBox.Show(vivod);
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+           Application.Current.Shutdown();
+        }
+
+        private void AuthorizationClick(object sender, RoutedEventArgs e)
+        {
+            if (NumbersOfIncorrectAuthorizations < 5)
+            {
+                try
+                {
+                    User user = db.User.ToList().Where(us => us.UserLogin == LoginTextBox.Text && us.UserPassword == PasswordTextBox.Text).FirstOrDefault();
+                    if (user != null)
+                    {
+                        MessageBox.Show(user.UserLogin);
+                        MessageBox.Show("Вы успешно авторизовались");
+                        MessageBox.Show("Ваша роль: " + user.Role.RoleName);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверные данные для входа");
+                        NumbersOfIncorrectAuthorizations++;
+
+                        TextBoxClear();
+                        if (NumbersOfIncorrectAuthorizations >= 5)
+                        {
+                            captchaWindow.Show();
+                            GenerateCaptcha();
+                            captchaWindow.CaptchaTextBlock.Text = CaptchaNumbers.ToString();
+                            MessageBox.Show("Вы потратили на авторизацию слишком много попыток");
+                        }
+                    }
+                    // MessageBox.Show("Успешная авторизация");
+                }
+                catch
+                {
+                    MessageBox.Show("Возникла непредвиденная ошибка");
+                }
+            }
+            else
+            {
+                captchaWindow.Show();
+                GenerateCaptcha();
+                captchaWindow.CaptchaTextBlock.Text = CaptchaNumbers.ToString();
+                MessageBox.Show("Сначала пройдите капчу!");
+            }
+            }
+
+            public void TextBoxClear()
+        {
+            PasswordTextBox.Text = "";
+            LoginTextBox.Text = "";
+        }
+
+        private void ProductsClick(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Hide();
+            productWindow.Show();
+            productWindow.ProductListView.ItemsSource = db.Product.ToList();
+        }
+    }
+}
---
+++ ProductDEmo/ProductWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProductDEmo
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
+    {
+        public static List<Product> productSortedList = MainWindow.db.Product.ToList();
+        public ProductWindow()
+        {
+            InitializeComponent();
+           // SortByDiscount.SelectedIndex = 0;
+        }
+        private void BackClick(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow.mainWindow.Show();
+        }
+
+        private void TextChange(object sender, TextChangedEventArgs e)
+        {
+            /* ProductListView.ItemsSource = MainWindow.db.Product.Where(prod => prod.ProductName.StartsWith(FindTextBox.Text)).ToList();
+             if (SortComboBox.SelectedIndex == 0)
+             {
+                 List<Product> products = MainWindow.db.Product.Where(prod => prod.ProductName.StartsWith(FindTextBox.Text)).ToList();
+                 ProductListView.ItemsSource = products.OrderByDescending(prod => prod.ProductCost);
+             }
+             if (SortComboBox.SelectedIndex == 1)
+             {
+                 List<Product> products = MainWindow.db.Product.Where(prod => prod.ProductName.StartsWith(FindTextBox.Text)).ToList();
+                 ProductListView.ItemsSource = products.OrderBy(prod => prod.ProductCost);
+             }*/
+            AddAllFilters();
+        }
+        public void AddAllFilters()
+        {
+            //ADdPoisk
+            productSortedList = productSortedList.Where(prod => prod.ProductName.StartsWith(FindTextBox.Text)).ToList();
+          // MessageBox.Show("COunt: " + productSortedList.Count);
+            //sort by first combobox
+             if (SortComboBox.SelectedIndex == 0)
+             {
+                 productSortedList = productSortedList.OrderByDescending(prod => prod.ProductCost).ToList();
+             }
+             if (SortComboBox.SelectedIndex == 1)
+             {
+                 productSortedList = productSortedList.OrderBy(prod => prod.ProductCost).ToList();
+             }
+            //sortByDiscount
+             if (SortByDiscount.SelectedIndex == 1)
+             {
+                 productSortedList = productSortedList.Where(prod => prod.ProductDiscountAmount>0 && prod.ProductDiscountAmount<10).ToList();
+             }
+             if (SortByDiscount.SelectedIndex == 2)
+             {
+                 productSortedList = productSortedList.Where(prod => prod.ProductDiscountAmount > 9.99 && prod.ProductDiscountAmount < 15).ToList();
+             }
+             if (SortByDiscount.SelectedIndex == 3)
+             {
+                 productSortedList = productSortedList.Where(prod => prod.ProductDiscountAmount > 14.99).ToList();
+             }
+            ProductListView.ItemsSource = productSortedList;
+            productSortedList= MainWindow.db.Product.ToList();
+        }
+        private void ChangeSelection(object sender, SelectionChangedEventArgs e)
+        {
+
+           /* if(SortComboBox.SelectedIndex == 0)
+            {
+                List<Product> products = MainWindow.db.Product.Where(prod => prod.ProductName.StartsWith(FindTextBox.Text)).ToList();
+                ProductListView.ItemsSource = products.OrderByDescending(prod => prod.ProductCost);
+            }
+            if (SortComboBox.SelectedIndex == 1)
+            {
+                List<Product> products = MainWindow.db.Product.Where(prod => prod.ProductName.StartsWith(FindTextBox.Text)).ToList();
+                ProductListView.ItemsSource = products.OrderBy(prod => prod.ProductCost);
+            }*/
+           AddAllFilters();
+        }
+
+        private void ChangeSelectionOfDiscountComboBox(object sender, SelectionChangedEventArgs e)
+        {
+           AddAllFilters();
+        }
+    }
+}
